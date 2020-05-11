493ca9944fac44f52d25e39a4973c583d883e0bd
David Brown <d.brown@novuminsights.com>
2020-05-11 06:50:44 +0100

    initial commit

--- src/constants/appConfig.js
+++ src/constants/appConfig.js
@@ -2,7 +2,7 @@ const date = new Date();
 const year = date.getFullYear();
 
 const APPCONFIG = {
-  brand: 'Novum Insights',
+  brand: 'Material',
   year,
   AutoCloseMobileNav: true,           // true, false. Automatically close sidenav on route change (Mobile only)
   customizer: true,                   // Boolean: true, false. Customizer will be removed completely when set to false
@@ -20,14 +20,14 @@ const APPCONFIG = {
   settings: {
     layout: '1',                      // String: 1, 2, 3, 4 and add your own
     boxedLayout: false,               // Boolean: true, false
-    fixedSidenav: true,               // Boolean: true, false
-    fixedHeader: true,                // Boolean: true, false
+    fixedSidenav: false,               // Boolean: true, false
+    fixedHeader: false,                // Boolean: true, false
     collapsedNav: false,              // Boolean: true, false
     offCanvasNav: false,              // Boolean: true, false
     offCanvasMobileNav: true,         // Boolean: true, false. Mobile only, by default, it's true (off canvas)
     sidenavWidth: 240,                // Number
     headerShadow: true,               // Boolean: true, false
-    sidenavShadow: true,             // Boolean: true, false
+    sidenavShadow: false,             // Boolean: true, false
     colorOption: '32',                // String: 11,12,13,14,15,16; 21,22,23,24,25,26; 31,32,33,34,35,36
     theme: 'light',                   // (WIP) String: light, gray, dark
   }
