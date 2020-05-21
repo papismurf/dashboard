4a1a2e21d29a62aa9e2a9b3dacb7faba94dbd569
David Brown <d.brown@novuminsights.com>
2020-05-21 13:04:51 +0100

    refact axios for newsfeed

--- src/routes/dashboard/components/LiveNewsFeed.js
+++ src/routes/dashboard/components/LiveNewsFeed.js
@@ -4,14 +4,21 @@ import APIKit from '../../../providers/APIKit';
 import QueueAnim from 'rc-queue-anim';
 import MaterialIcon from 'components/MaterialIcon';
 
+/*
 const initialState = {
-  market: 'market',
-  key: 'key',
+  market: 'blockchain',
+  key: '5840e9e52720764bf18c961d5f9efe3672e00332',
 }
 this.key = initialState.key
 this.market = initialState.market
-const articles = axios.get(APIKit.baseURL + '/articles/')
+const articles = axios({
+  method: 'get',
+  url: APIKit.baseURL + '/articles/',
+  headers: {'Authorization': 'Token ' + this.key},
+  params: {'limit': 20, 'offset': 0, 'market': this.market},
+})
   .then()
+*/
 
 
 
@@ -27,13 +34,13 @@ const Page = () => {
           {
             articles.map((article, i) => (
               <article key={i.toString()} className="blog-card flex-item border-0 mdc-elevation--z1 mb-4">
-                <a href={article.link}>
+                <a href={articles.published_at}>
                   <img src={article.img} alt="blog cover"/>
                 </a>
                 <div className="blog-card__body">
                   <span className="blog-card__date">{article.date}</span>
                   <h4 className="blog-card__title">{article.content}</h4>
-                  <a href={DEMO.link} className="link-animated-hover link-hover-v1 text-primary">Read More <MaterialIcon icon="arrow_forward" /></a>
+                  <a href={article.link} className="link-animated-hover link-hover-v1 text-primary">Read More <MaterialIcon icon="arrow_forward" /></a>
                 </div>
               </article>
             ))
