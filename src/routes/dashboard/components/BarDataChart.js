86022683b8a419a4b20d7b011e9bc28e1f3e6d5d
David Brown <d.brown@novuminsights.com>
2020-06-07 22:11:16 +0100

    added stack barchart to dashboard

--- src/routes/dashboard/components/BarDataChart.js
+++ src/routes/dashboard/components/BarDataChart.js
@@ -1,74 +1,136 @@
 import React from 'react';
-import { Bar } from 'react-chartjs-2';
-import { defaults } from 'react-chartjs-2';
+import ReactEcharts from 'echarts-for-react';
+import 'echarts/theme/macarons';
 import CHARTCONFIG from 'constants/chartConfig';
 
-const backgroundColors = [
-  'rgba(255, 99, 132, 1.0)',
-  'rgba(54, 162, 235, 1.0)',
-  'rgba(255, 206, 86, 1.0)',
-  'rgba(75, 192, 192, 1.0)',
-  'rgba(153, 102, 255, 1.0)',
-  'rgba(255, 159, 64, 1.0)',
-  'rgba(35, 169, 80, 1.0)',
-  'rgba(67, 1, 49, 1.0)',
-  'rgba(27, 151, 190, 1.0)',
-  'rgba(117, 177, 169, 1.0)',
-  'rgba(189, 79, 144, 1.0)',
-  'rgba(149, 163, 17, 1.0)',
-  'rgba(181, 29, 156, 1.0)',
-  'rgba(13, 238, 134, 1.0)',
-  'rgba(71, 16, 78, 1.0)',
-  'rgba(234, 166, 65, 1.0)',
-  'rgba(44, 190, 40, 1.0)',
-  'rgba(217, 247, 134, 1.0)',
-  'rgba(174, 119, 212, 1.0)',
-  'rgba(91, 60, 241, 1.0)',
-  'rgba(203, 212, 162, 1.0)',
-  'rgba(189, 137, 23, 1.0)',
-  'rgba(74, 7, 167, 1.0)',
-  'rgba(115, 206, 128, 1.0)',
-  'rgba(249, 75, 88, 1.0)'
-];
+let bar4 = {};
 
-const data = {
-  labels: ['2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020'],
-  datasets: [
+bar4.option = {
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: {
+      type: 'shadow'
+    }
+  },
+  legend: {
+    data: ['Financial Markets', 'Financial Services', 'Blockchain Ecosystem', 'IT/Analytics', 'Traditional Media',
+            'Social Media', 'Marketing', 'Healthcare'],
+    textStyle: {
+      color: CHARTCONFIG.color.text
+    }
+  },
+  toolbox: {
+    show: true,
+    feature: {
+      saveAsImage: {show: true, title: 'save'}
+    }
+  },
+  calculable: true,
+  xAxis: [
     {
-      label: 'Number of Companies over time',
-      backgroundColor: backgroundColors,
-      borderColor: 'rgba(203, 212, 162, 1.0)',
-      borderWidth: 1,
-      hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-      hoverBorderColor: 'rgba(203, 212, 162, 1.0)',
-      data: [65, 59, 80, 81, 56, 55, 40, 76, 20, 23, 25],
-      options: {
-        responsive: true,
-        legend: {
-          labels: {
-            fontcolor: 'blue',
-            defaultFontFamily: 'Helvetica',
-          }
+      type: 'value',
+      axisLabel: {
+        textStyle: {
+          color: CHARTCONFIG.color.text
+        }
+      },
+      splitLine: {
+        lineStyle: {
+          color: CHARTCONFIG.color.splitLine
         }
       }
     }
+  ],
+  yAxis: [
+    {
+      type: 'category',
+      data: ['2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020'],
+      axisLabel: {
+        textStyle: {
+          color: CHARTCONFIG.color.text
+        }
+      },
+      splitLine: {
+        lineStyle: {
+          color: CHARTCONFIG.color.splitLine
+        }
+      },
+      splitArea: {
+        show: true,
+        areaStyle: {
+          color: CHARTCONFIG.color.splitArea
+        }
+      }
+    }
+  ],
+  series: [
+    {
+      name: 'Financial Markets',
+      type: 'bar',
+      stack: 'Sum',
+      itemStyle: { normal: {label: {show: false, position: 'insideRight'}}},
+      data: [0, 0, 9, 37, 36, 64, 297, 930, 434, 107]
+    },
+    {
+      name: 'Financial Services',
+      type: 'bar',
+      stack: 'Sum',
+      itemStyle: { normal: {label: {show: false, position: 'insideRight'}}},
+      data: [0, 0, 9, 23, 50, 64, 193, 536, 197, 59]
+    },
+    {
+      name: 'Blockchain Ecosystem',
+      type: 'bar',
+      stack: 'Sum',
+      itemStyle: { normal: {label: {show: false, position: 'insideRight'}}},
+      data: [0, 0, 0, 11, 17, 46, 132, 291, 114, 36]
+    },
+    {
+      name: 'IT/Analytics',
+      type: 'bar',
+      stack: 'Sum',
+      itemStyle: { normal: {label: {show: false, position: 'insideRight'}}},
+      data: [0, 0, 0, 0, 9, 27, 84, 304, 127, 31]
+    },
+    {
+      name: 'Traditional Media',
+      type: 'bar',
+      stack: 'Sum',
+      itemStyle: { normal: {label: {show: false, position: 'insideRight'}}},
+      data: [0, 0, 0, 3, 0, 15, 53, 164, 41, 8]
+    },
+    {
+      name: 'Social Media',
+      type: 'bar',
+      stack: 'Sum',
+      itemStyle: { normal: {label: {show: false, position: 'insideRight'}}},
+      data: [0, 0, 0, 11, 5, 10, 42, 145, 32, 6]
+    },
+    {
+      name: 'Marketing',
+      type: 'bar',
+      stack: 'Sum',
+      itemStyle: { normal: {label: {show: false, position: 'insideRight'}}},
+      data: [1, 0, 0, 0, 2, 1, 31, 125, 36, 11]
+    },
+    {
+      name: 'Healthcare',
+      type: 'bar',
+      stack: 'Sum',
+      itemStyle: { normal: {label: {show: false, position: 'insideRight'}}},
+      data: [0, 0, 0, 0, 0, 5, 31, 120, 39, 9]
+    },
   ]
 };
 
-export default class BarDataChart extends React.Component {
+const BarDataChart = () => (
+  <div className="box box-default mb-4">
+    <div className="box-header">Number of Companies Over Time</div>
+    <div className="box-body">
+      <ReactEcharts option={bar4.option} theme={"macarons"} />
+    </div>
+  </div>
+)
 
-  render() {
-    return (
-      <div>
-        <Bar
-          data={data}
-          width={100}
-          height={45}
-          options={{
-            maintainAspectRatio: true
-          }}
-        />
-      </div>
-    );
-  }
-};
+export default BarDataChart;
+
